6d84b16bd1807f05bef56da88a6e9578fab3bf53
Nathan Stork <nejc.stork@hotmail.nl>
2022-09-18 13:06:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from scripts import downloader, merger
 def main():
     urls = [
         'https://streaming10.ur.se/urplay/_definst_/mp4:se/207000-207999/207811-4.mp4/media_${num:0}.ts',  # Example URL
-        'https://streaming10.ur.se/urplay/_definst_/mp4:se/207000-207999/207810-5.mp4/media_${num:0}.ts',  # Example URL
-        'https://streaming10.ur.se/urplay/_definst_/mp4:se/207000-207999/207813-8.mp4/media_${num:0}.ts'  # Example URL
+        'https://streaming10.ur.se/urplay/_definst_/mp4:se/207000-207999/207810-5.mp4/media_${num:0}.ts'  # Example URL
     ]
 
     for url in urls:
